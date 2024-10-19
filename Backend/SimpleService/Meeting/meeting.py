086f478b2f1049447b9ccb5d90529a12ab05fb51
mondojondo <mondojondo@gmail.com>
2024-10-19 23:31:17 +0800

    updated get_meetings filter for date

--- Backend/SimpleService/Meeting/meeting.py
+++ Backend/SimpleService/Meeting/meeting.py
@@ -50,21 +50,25 @@ def create_meeting():
 # retrieve meeting
 @app.route('/meeting', methods=['GET'])
 def get_meetings():
+    data = request.get_json()
+
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor(dictionary=True)
 
     query = ("SELECT * FROM Meeting where 1=1")
 
-    Meeting_ID = request.args.get('Meeting_ID')
-    Created_By = request.args.get('Created_By')
-    Date = request.args.get('Date')
+    Meeting_ID = data.get('Meeting_ID')
+    Created_By = data.get('Created_By')
+    Date = data.get('Date')
 
     if Meeting_ID:
         query += f" AND Meeting_ID = {Meeting_ID}"
     if Created_By:
         query += f" AND Created_By = {Created_By}"
     if Date:
-        query += f" AND Date = {Date}"
+        date_object = datetime.strptime(Date, "%Y-%m-%d")
+        formatted_date = date_object.strftime("%Y-%m-%d")
+        query += f" AND Date = '{formatted_date}'"
 
     values = ()
 
