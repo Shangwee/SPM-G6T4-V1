13851a6b6835ff6e522e69edf4377a4ff8e6af54
Shang Wee <hoshangwee0911@gmail.com>
2024-10-24 02:44:36 +0800

    revert notification to use compose SQL

--- Backend/SimpleService/Notification/src/config/db.js
+++ Backend/SimpleService/Notification/src/config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 // Create a pool of connections to the MySQL database
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'spmg6t4.duckdns.org',
-    user: process.env.DB_USER || 'remote',
-    password: process.env.DB_PASSWORD || 'P@ssw0rd',
+    host: process.env.DB_HOST || 'notification_db',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'notifications',
     waitForConnections: true,
     connectionLimit: 10,
