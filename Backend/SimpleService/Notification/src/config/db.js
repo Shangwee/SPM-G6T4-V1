d325d50b3fe1a1a087dc3ccfc767c38477e2a669
Shang Wee <hoshangwee0911@gmail.com>
2024-10-24 02:37:53 +0800

    Merge sql to external server

--- Backend/SimpleService/Notification/src/config/db.js
+++ Backend/SimpleService/Notification/src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 // Create a pool of connections to the MySQL database
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'notifications_db',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'notifications_db',
+    host: process.env.DB_HOST || 'spmg6t4.duckdns.org',
+    user: process.env.DB_USER || 'remote',
+    password: process.env.DB_PASSWORD || 'P@ssw0rd',
+    database: process.env.DB_NAME || 'notifications',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
