77a3d56a9216a501dfc32efe007a9aa26e68fcb5
Shane Ho <145674908+FS0-range@users.noreply.github.com>
2024-10-28 20:36:39 +0800

    Modified homepage to show team members instead of department

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
-
+const NOTIFICATION_API = import.meta.env.VITE_NOTIFICATION_API;
 let socket = null;
 
 export const getSocket = () => {
   // Check if the socket instance already exists
   if (!socket) {
     // If not, create a new socket connection
-    socket = io("http://localhost:5005", {
+    socket = io(`${NOTIFICATION_API}`, {
       // Optionally add connection options
       reconnectionAttempts: 5, // Set max reconnection attempts
       transports: ["websocket"], // Force WebSocket only
